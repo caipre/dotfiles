beb43b6c4c5f3f4b74d993052e711ec6ea42d1d6
Nick Platt <platt.nicholas@gmail.com>
2015-10-15 13:19:14 -0400

    Remove hostname from rprompt

--- zsh/prompts.zsh
+++ zsh/prompts.zsh
@@ -4,13 +4,11 @@
 autoload -U colors && colors
 function short_prompt () {
    PROMPT="%{$fg_bold[white]%}%(!.#.$)%{$reset_color%} "
-   RPROMPT="%m"
    LONG_PROMPT=false
 }
 
 function long_prompt () {
    PROMPT="%{$fg_bold[white]%}%(!.#.$)%{$reset_color%} %{$fg_bold[black]%}%~%{$reset_color%} "
-   RPROMPT="%m"
    LONG_PROMPT=true
 }
 long_prompt
