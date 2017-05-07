b879dbe1510c9ca88c5b04324d9bc731a5127d81
Nick Platt <platt.nicholas@gmail.com>
2017-05-07 19:04:23 +0200

    Small changes to small functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,20 +2,22 @@
 # Author: Nick Platt
 
 function mkcd () {
-   if [[ -z $* ]]; then
-      echo 'Usage: mkcd <dir>';
-      echo '   Make a directory and move into it';
+   if [[ $# -ne 1 ]]; then
+      echo 'usage: mkcd <dir>'
+      echo '   make a directory and move into it'
    else
-      mkdir -p "$1"; cd "$1";
+      mkdir -p "$1" && cd "$1"
    fi
 }
 
 function mkmv () {
-   if [[ -z $* ]]; then
-      echo 'Usage: mkmv <dir> <file> [ <file> ... ]';
-      echo '   Make a directory and move file(s) into it';
+   if [[ $# -lt 2 ]]; then
+      echo 'usage: mkmv <dir> <file>...'
+      echo '   make a directory and move file(s) into it'
    else
-      mkdir -p "$1"; mv "$@[2,-1]" "$1";
+      mkdir -p "$1" && mv "$@[2,-1]" "$1"
+   fi
+}
 
 function copy () {
    if [[ $# -ne 1 ]]; then
@@ -74,5 +76,5 @@ function clrs () {
       printf "\033[38;5;${code}m%03d\033[39m " $code
    done
 
-   print;
+   print
 }
