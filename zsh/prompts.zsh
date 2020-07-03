f2adf522e590c263723d65d9ebf17fe796e5a60e
Nick Platt <platt.nicholas@gmail.com>
2020-07-02 23:45:14 -0400

    support pyenv in zsh prompt

--- zsh/prompts.zsh
+++ zsh/prompts.zsh
@@ -1,11 +1,19 @@
 autoload -U colors && colors
 function short_prompt () {
-   PROMPT="%{$fg_bold[white]%}%(!.#.$)%{$reset_color%} "
+   local pyenv=''
+   if [[ -n "${PYENV_VERSION}" ]]; then
+      #pyenv="${PYENV_VERSION} "
+   fi
+   PROMPT="$pyenv%{$fg_bold[white]%}%(!.#.$)%{$reset_color%} "
    LONG_PROMPT=false
 }
 
 function long_prompt () {
-   PROMPT="%{$fg_bold[white]%}%(!.#.$)%{$reset_color%} %{$fg_bold[black]%}%~%{$reset_color%} "
+   local pyenv=''
+   if [[ -n "${PYENV_VERSION}" ]]; then
+      #pyenv="${PYENV_VERSION} "
+   fi
+   PROMPT="$pyenv%{$fg_bold[white]%}%(!.#.$)%{$reset_color%} %{$fg_bold[black]%}%~%{$reset_color%} "
    LONG_PROMPT=true
 }
 long_prompt
